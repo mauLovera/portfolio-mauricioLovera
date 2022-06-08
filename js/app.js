7acd8c9fb5738b7478caef423fbe05b6e911f773
mauLovera <mauricioloveraedu@gmail.com>
2022-06-08 14:34:50 -0400

    before navbar

---
+++ js/app.js
@@ -0,0 +1,24 @@
+// Variables
+
+// Constants
+
+// CERs 
+
+const hamBtn = document.getElementById('Hamburger')
+const xBtn = document.getElementById('Header-nav-x')
+
+// Event Listeners
+
+hamBtn.addEventListener(`click`, openNavBar)
+xBtn.addEventListener(`click`, closeNavBar)
+
+
+// Functions
+
+function openNavBar() {
+  
+}
+
+function closeNavBar() {
+  
+}
